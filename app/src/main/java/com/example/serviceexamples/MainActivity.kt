7d546b40e65bc4b9aed20c3b54b1c2fe2d96455f
linoop <linoop@technowavegroup.com>
2022-05-09 17:09:31 +0530

    myservice added

--- app/src/main/java/com/example/serviceexamples/MainActivity.kt
+++ app/src/main/java/com/example/serviceexamples/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.serviceexamples
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    //private lateinit var myService: MyService
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //myService = MyService()
+        findViewById<Button>(R.id.startService).setOnClickListener {
+          Intent(this, MyService::class.java).also {
+              startService(it)
+          }
+        }
     }
 }
